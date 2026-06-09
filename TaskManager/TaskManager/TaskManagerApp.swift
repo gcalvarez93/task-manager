4ebfc42bb11e9d5aff05cf7ae3916e995a07222f
Gabriel Castro <gcalvarez93@gmail.com>
2026-06-09 20:05:46 +0200

    feat: initial Swift iOS project setup - TaskManager

---
+++ TaskManager/TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Gabriel Castro on 09/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
